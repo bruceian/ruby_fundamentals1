dda717d11266fb890e18838ee7c7d8ba2f66b4fa
Ian Bruce <brucegordonian@gmail.com>
2016-08-09 14:05:38 -0400

    Tips and string interpolation

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+#problem 1
+puts 55 * 0.20
+
+#problem 2
+puts "#{55 * 0.20} dollars is a good tip on 55 dollars"
+
+#problem 3
+puts "The result of 45628 multiplied by 7839 is #{45628 * 7839}"
+
+#problem 4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+#My guess before running... the value is true.
+#And I was right!
